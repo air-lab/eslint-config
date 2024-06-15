833dad3247d5c5553e180a4e83f9cdc372e73af1
Yan Zhabin <iyanello@gmail.com>
2024-06-15 16:42:07 -0700

    Update dependencies.

--- postcss.d.ts
+++ postcss.d.ts
@@ -1,4 +1,4 @@
 import { AcceptedPlugin } from 'postcss'
 
-export const parser: string;
-export const plugins: AcceptedPlugin[];
+export const parser: string
+export const plugins: AcceptedPlugin[]
