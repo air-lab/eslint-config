1107230329f20cd4a736f17584d4d526c1303ba7
Yan Zhabin <iyanello@gmail.com>
2024-06-15 16:42:07 -0700

    Update dependencies.

--- postcss.js
+++ postcss.js
@@ -13,20 +13,5 @@ module.exports = {
         require("postcss-flexbugs-fixes"),
         require("postcss-input-range"),
         require("postcss-scrollbar"),
-        require("postcss-pxtorem")({
-            "replace": true,
-            "propList": [
-                "*",
-                "!outline",
-                "!border-width",
-                "!border",
-                "!border-left",
-                "!border-right",
-                "!border-top",
-                "!border-bottom",
-                "!border-radius",
-                "!letter-spacing"
-            ]
-        }),
     ]
 }
